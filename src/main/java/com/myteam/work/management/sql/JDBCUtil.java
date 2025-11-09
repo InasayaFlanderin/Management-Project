8686ef33c374fad3ec214e751962fd11c4d916e1
Duong Duong <duongduong190906@gmail.com>
2025-11-09 15:58:45 +0700

    slf4j, log4j2

--- src/main/java/com/myteam/work/management/sql/JDBCUtil.java
+++ src/main/java/com/myteam/work/management/sql/JDBCUtil.java
@@ -3,23 +3,28 @@ package com.myteam.work.management.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class JDBCUtil{
-    public JDBCUtil(){
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JDBCUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(JDBCUtil.class);
+
+    public JDBCUtil() {
 
     }
 
-    public static Connection getConnection()
-    {
+    public static Connection getConnection() {
         Connection connection = null;
 
-        try{
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/doanoop";
-        String username = "root";
-        String password = "";
-        connection = DriverManager.getConnection(url, username, password);
-        }catch(Exception e){
-            e.printStackTrace();
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/doanoop";
+            String username = "postgres";
+            String password = "";
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            log.error("Error getting connection: ", e);
         }
 
         return connection;
@@ -28,10 +33,10 @@ public class JDBCUtil{
     public static void closeConnection(Connection c) {
 
         try {
-            if(c != null && !c.isClosed())
+            if (c != null && !c.isClosed())
                 c.close();
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("Error closing connection: ", e);
         }
     }
 
